81b99ed9ce9dc8435646b104d1ebf94247645ef5
Mhmmdalfn1502 <malfan227@gmail.com>
2026-04-10 07:33:00 +0700

    Fix Update - Bismillah

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,4 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // ⬅️ Tambahkan ini
-  },
-  // Tambahkan opsi lain di sini jika perlu
-};
-
+const nextConfig: NextConfig = {};
 export default nextConfig;
